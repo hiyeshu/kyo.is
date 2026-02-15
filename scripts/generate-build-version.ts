9a02faf33e05c771201d1c33a588f2c14ec86a80
shuye <hiyeshu@qq.com>
2026-02-15 13:25:55 +0800

    chore: trigger vercel redeploy

--- scripts/generate-build-version.ts
+++ scripts/generate-build-version.ts
@@ -16,7 +16,7 @@ import { fileURLToPath } from 'url';
 // ============================================================================
 const MAJOR = 1;
 const MINOR = 0;
-const PATCH = 57;
+const PATCH = 58;
 // ============================================================================
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
